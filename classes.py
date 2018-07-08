4783b5eb63bdc31bf84b798033242e327790552e
Zaharov-PC\Zaharov <averlarque@yandex.by>
2018-07-08 15:09:11 +0300

    init commit

---
+++ classes.py
@@ -0,0 +1,58 @@
+
+class UserStory:
+    """Class for storing and transforming user stories"""
+    def __init__(self, object):
+        self.feature_index = object["feature_index"] + 1
+        self.story_index = object["story_index"] + 1
+        self.title = object["title"]
+        self.description = object["description"]
+        self.criteria = object["criteria"]
+
+    def create_layout(self):
+        # h2 = '<h2 class="auto-cursor-target">Пользовательские истории</h2> \n'
+        h3 = '\n<h3>N.{0}-US-{1}. {2}</h3>\n'.format(self.feature_index, self.story_index, self.title)
+        table_open = '<table><colgroup><col /><col /></colgroup> \n <tbody> \n'
+        description_row = '<tr> \n <th>Описание</th> \n <td>{}</td></tr> \n'.format(self.description)
+        criteria = self._iterate_criteria()
+        criteria_row = '<tr>\n<th>Критерии приемки</th>\n<td><ol>\n{}</ol></td></tr>'.format(criteria)
+        uc_row = '\n<tr>\n <th>UC</th>\n<td></td></tr>'
+        fc_row = '\n<tr>\n<th>FR</th>\n<td></td></tr>'
+        table_close = '\n</tbody></table>\n'
+
+        result = h3 + table_open + description_row + criteria_row + uc_row + fc_row + table_close
+
+        return result
+    
+    def _iterate_criteria(self):
+        result = ''
+        for item in self.criteria:
+            result += '<li>{}</li>\n'.format(item)
+        return result
+
+class FuncRequirements:
+    """Class for storing and transforming functional requirements """
+    def __init__(self, object):
+        self.func_reqs = object
+
+    def _iterate_funcs(self):
+        result = ''
+        for f in self.func_reqs:
+            fe_index = f['feature_index'] + 1
+            fu_index = f['func_index'] + 1
+            fu_title = f['func_title']
+            fu_des = f['description']
+            fu_des_items = ''
+            for f in fu_des:
+                fu_des_items += '<li>{}</li>\n'.format(f)
+            result += '<tr>\n<td>N.{0}-FR-{1}</td>\n<td>{2}</td>\n<td><ol>\n{3}</ol></td>\n</tr>\n'.format(fe_index, fu_index, 
+            fu_title, fu_des_items)
+
+        return result
+
+    def create_layout(self):
+        table_open = '\n<table class="wrapped"><colgroup><col /><col /><col /></colgroup>\n<tbody>\n'
+        header_row = '<tr>\n<th>ID</th>\n<th>Название</th>\n<th>Описание</th>\n</tr>\n'
+        freqs = self._iterate_funcs()
+        table_close = '</tbody></table>\n'
+        result = table_open + header_row + freqs + table_close
+        return result
---
+++ script.py
@@ -0,0 +1,96 @@
+import xmind
+from classes import UserStory, FuncRequirements
+import pathlib
+
+# Global variables
+path = "assets\\"
+
+def get_user_stories(feature):
+    """
+    param: xmind.core.topic.TopicElement object
+    return: list
+    Supportive function to transform user stories from xmind to html layout
+    """
+    # Create list for output
+    result = []
+    # Get feature's index
+    feature_index = feature.getIndex()
+    # Get through label identifying user stories
+    user_stories = feature.getSubTopicByIndex(0)
+    # Check if not empty, else return empty list
+    if user_stories:
+        # Get list of stories' titles
+        stories = user_stories.getSubTopics()
+        # Itetare through each story and create data structure
+        for story in stories:
+            description = story.getSubTopicByIndex(0)
+            user_story = {
+                "feature_index": feature_index,
+                "story_index": story.getIndex(),
+                "title": story.getTitle(),
+                "description": description.getTitle(),
+                "criteria": [x.getTitle() for x in description.getSubTopics()]
+            }
+            # Call UserStory class, pass a story and transform it to layout
+            result.append(UserStory(user_story).create_layout())
+    return result
+       
+def get_func_reqs(feature):
+    """
+    param: xmind.core.topic.TopicElement object
+    return: str
+    Supportive function to transform functional requirements from xmind to html layout
+    """
+    # Create list for reqs
+    reqs = []
+    # Get feature's index
+    feature_index = feature.getIndex()
+    # Get through label identifying functional requirements
+    functionals = feature.getSubTopicByIndex(1)
+    # check in not empty, else return an empty list
+    if functionals:
+        # Get list of freqs' titles
+        func_reqs = functionals.getSubTopics()
+        # Itetare through each frequirement and create data structure
+        for f in func_reqs:
+            func_req = {
+                "feature_index": feature_index,
+                "func_index": f.getIndex(),
+                "func_title": f.getTitle(),
+                "description": [x.getTitle() for x in f.getSubTopics()]
+            }
+            reqs.append(func_req)
+        # Call FuncRequirement class, pass a list of reqs and transform it to layout
+        return FuncRequirements(reqs).create_layout()
+    else:
+        return reqs
+
+def generate_features(name, section_index=0):
+    # Load .xmind file
+    w = xmind.load(path + "{}.xmind".format(name))
+    # Load firt (primary sheet)
+    sheet=w.getPrimarySheet()
+    # Get root topic
+    root = sheet.getRootTopic()
+    # Get required section
+    section = root.getSubTopicByIndex(section_index)
+    # Create folder for a section to keep features
+    section_folder_path = path + section.getTitle()
+    pathlib.Path(section_folder_path).mkdir(exist_ok=True) 
+    # Extract features and start iterating
+    features = section.getSubTopics()
+
+    for feature in features:
+        feature_name = str(feature.getIndex() + 1) + '.' + feature.getTitle()
+        output = '<h2>Пользовательские истории</h2>'
+        stories = get_user_stories(feature)
+        for story in stories:
+            output += story
+        output += '<h2>Функциональные требования</h2>'
+        func_reqs = get_func_reqs(feature)
+        output += str(func_reqs)
+        # Write output in a file
+        with open(section_folder_path + '\\' + feature_name + ".txt", 'w') as f:
+            f.write(output)
+                  
+generate_features("srs")
